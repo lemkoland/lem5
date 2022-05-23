acdbf1174809b939e16aad5cdd87ee06d761f3de
lemkoland <98834856+lemkoland@users.noreply.github.com>
2022-05-23 18:35:39 +0200

    Update WordPressPage.vue

--- src/templates/WordPressPage.vue
+++ src/templates/WordPressPage.vue
@@ -1,5 +1,10 @@
 <template>
   <Layout>
+    <div class="in">
+      <div class="inin">
+
+      </div>
+    </div>
     <div class="page-glass">
 
 
@@ -60,11 +65,40 @@
 </page-query>
 
 <script>
+import { TimelineLite, TweenMax, gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin.js';
+gsap.registerPlugin(ScrollToPlugin );
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+gsap.registerPlugin(ScrollTrigger);
+
 export default {
   metaInfo () {
     return {
       title: this.$page.wordPressPage.title
     }
+  },
+  mounted() {
+    const tl = new TimelineLite();
+
+
+        tl.to('.in', 1, {delay: 1.0,  clipPath: 'polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)', easing: 'easeOutQuint'}, '-=.5')
+        .to('.header', 1.5, { clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', easing: 'easeOutQuint'});
+
+
+    var color = ['#5d237a', '#d17fb4', '#505ce5', '#62e564', '#6121EB', '#8B00A1', '#834D9B',
+    '#D04ED6', '#1CD8D2', '#93EDC7', '#EBB752', '#E16BEB', '#B5CCFF', '#1A3091', '#7594C9',
+  '#784C39', '#B07156', '#AE605F', '#AB4E68', '#7F4357', '#533745', '#A8948A', '#3B302B',
+'#9D9171', '#554B30', '#9E8234', '#998C69', '#4E4838', '#845440', '#845440', '#DB4F4F',
+'#4B1A1A', '#473527', '#552600','#A87E5D' ];
+
+
+    var c1 = Math.floor(Math.random() * color.length);
+    var cca = color[c1];
+    gsap.to('body', .7, { backgroundColor: cca});
+
+    var c2 = Math.floor(Math.random() * color.length);
+    var cca2 = color[c2];
+    gsap.to('.in', .7, { backgroundColor: cca2});
   }
 }
 </script>
