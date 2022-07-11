eda0954233ceadf2e18497b587f65c5e3d791eb2
lemkoland <98834856+lemkoland@users.noreply.github.com>
2022-07-11 16:24:55 +0200

    Update WordPressCategory.vue

--- src/templates/WordPressCategory.vue
+++ src/templates/WordPressCategory.vue
@@ -110,32 +110,32 @@ export default {
       '#784C39', '#B07156', '#AE605F', '#AB4E68', '#7F4357', '#533745', '#A8948A', '#3B302B',
     '#9D9171', '#554B30', '#9E8234', '#998C69', '#4E4838', '#845440', '#845440', '#DB4F4F',
     '#4B1A1A', '#473527', '#552600','#A87E5D' ];
-    
+
             var c2 = Math.floor(Math.random() * color.length);
             var cca2 = color[c2];
             gsap.to('.in', .7, { backgroundColor: cca2});
 
-    var sections = gsap.utils.toArray('.trigerPost');
-
-    sections.forEach((section) => {
-      var color = ['#5d237a', '#d17fb4', '#505ce5', '#62e564', '#6121EB', '#8B00A1', '#834D9B',
-      '#D04ED6', '#1CD8D2', '#93EDC7', '#EBB752', '#E16BEB', '#B5CCFF', '#1A3091', '#7594C9',
-    '#784C39', '#B07156', '#AE605F', '#AB4E68', '#7F4357', '#533745', '#A8948A', '#3B302B',
-  '#9D9171', '#554B30', '#9E8234', '#998C69', '#4E4838', '#845440', '#845440', '#DB4F4F',
-  '#4B1A1A', '#473527', '#552600','#A87E5D' ];
-
-
-      var c1 = Math.floor(Math.random() * color.length);
-      var cca = color[c1];
-
-      gsap.to('body', .3, { backgroundColor: cca,
-
-        scrollTrigger: {
-          start: "bottom center",
-        	trigger: section,
-          scrub: true
-        }
-    });
+  //   var sections = gsap.utils.toArray('.trigerPost');
+  //
+  //   sections.forEach((section) => {
+  //     var color = ['#5d237a', '#d17fb4', '#505ce5', '#62e564', '#6121EB', '#8B00A1', '#834D9B',
+  //     '#D04ED6', '#1CD8D2', '#93EDC7', '#EBB752', '#E16BEB', '#B5CCFF', '#1A3091', '#7594C9',
+  //   '#784C39', '#B07156', '#AE605F', '#AB4E68', '#7F4357', '#533745', '#A8948A', '#3B302B',
+  // '#9D9171', '#554B30', '#9E8234', '#998C69', '#4E4838', '#845440', '#845440', '#DB4F4F',
+  // '#4B1A1A', '#473527', '#552600','#A87E5D' ];
+  //
+  //
+  //     var c1 = Math.floor(Math.random() * color.length);
+  //     var cca = color[c1];
+  //
+  //     gsap.to('body', .3, { backgroundColor: cca,
+  //
+  //       scrollTrigger: {
+  //         start: "bottom center",
+  //       	trigger: section,
+  //         scrub: true
+  //       }
+  //   });
 
 
     })
